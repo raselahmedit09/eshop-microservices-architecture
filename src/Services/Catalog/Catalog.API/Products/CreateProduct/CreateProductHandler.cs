0ef8a8742671ae5fd8f3728ef47ffaae6ae252c0
raselahmedit09 <rasel.ict09@gmail.com>
2025-12-11 15:58:45 +0600

    test commit

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -4,7 +4,6 @@ using Catalog.API.Models;
 namespace Catalog.API.Products.CreateProduct
 {
     // Application logic layer
-
     public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
     : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
