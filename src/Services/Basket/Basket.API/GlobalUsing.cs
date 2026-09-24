117223bd38485a2c8d8905b78d57bda0a01ea5d0
raselahmedit09 <rasel.ict09@gmail.com>
2026-09-24 12:01:18 +0600

    Add Basket microservice with API, Docker, and PostgreSQL
    Introduce Basket.API project using Carter, MediatR, Marten, and FluentValidation. Implement endpoints for basket CRUD operations with handlers, validators, and repository pattern. Add Docker support for Basket.API and basketdb in docker-compose files. Update solution file and add configuration and Dockerfile for Basket.API.

---
+++ src/Services/Basket/Basket.API/GlobalUsing.cs
@@ -0,0 +1,6 @@
+﻿global using Basket.API.Exceptions;
+global using Basket.API.Models;
+global using BuildingBlocks.Behaviors;
+global using BuildingBlocks.Exceptions;
+global using Carter;
+global using Marten;
